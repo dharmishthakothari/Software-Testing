52d0667cca76e0dd1cfbf0b4f1241e0051d11e0e
dharmishthakothari <dharmishtha.only@gmail.com>
2026-04-10 13:34:07 +0530

    Add files via upload

---
+++ 18_NOV_AT_TTS_12.30-1.30/Java/HashMapExample.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("str1", "dharmishtha");
+        map.put("str2", "prayusha");
+        map.put(1, "Maitri");
+        map.put(1, "Tops");
+        System.out.println(map);
+    }
+}
