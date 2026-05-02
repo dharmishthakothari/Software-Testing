86db8f5e68a692b0dafbc363d2db1d0ccd566f06
dharmishthakothari <dharmishtha.only@gmail.com>
2026-05-02 13:42:39 +0530

    Add files via upload

--- 18_NOV_AT_TTS_12.30-1.30/WebDriver/Main.java
+++ 18_NOV_AT_TTS_12.30-1.30/WebDriver/Main.java
@@ -1,13 +1,36 @@
 package com.example;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https:\\www.google.com");
 
+    public static void main(String[] args) throws Exception {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.manage().window().maximize();
+        // driver.findElement(By.id("datepicker")).sendKeys("02/20/2026");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // js.executeScript("window.scrollBy(0,1500)");
+        // driver.findElement(By.id("txtDate")).click();
+        // driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/table/tbody/tr[1]/td[7]/a")).click();
+
+        // driver.findElement(By.id("start-date")).sendKeys("01-01-2026");
+        // driver.findElement(By.id("end-date")).click();
+
+        Actions action = new Actions(driver);
+        // WebElement ele = driver.findElement(By.cssSelector("#HTML10 >
+        // div.widget-content > button"));
+        // action.doubleClick(ele);
+        // action.build().perform();
+
+        WebElement ele = driver.findElement(By.id("draggable"));
+        WebElement ele1 = driver.findElement(By.id("droppable"));
+        action.dragAndDrop(ele, ele1);
+        action.build().perform();
     }
 }
